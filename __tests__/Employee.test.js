3846da40bd3ecbfeaa9449a552b69be97cf6fda6
James Barry <jbm03003@gmail.com>
2022-02-02 13:04:24 -0500

    finished running basic passing tests in Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,27 @@
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee.js');
 
-test('Can create Employee object', () => {
-    const e = new Employee();
-    expect(typeof(e)).toBe('object');
-}); 
+test('Will create Employee object', () => {
+    const newEmp = new Employee();
+    expect(typeof(newEmp)).toBe('object');
+});
+
+test('Will get the name from class constructor', () => {
+    const name = 'Sue';
+    const newEmp = new Employee(name);
+    expect(newEmp.name).toBe(name);
+})
+
+test('Will get the ID from class constructor', () => {
+    const testEx = 100;
+    const newEmp = new Employee('Foo', testEx);
+    expect(newEmp.id).toBe(testEx);
+});
+
+test('Will get the Email from class constructor', () => {
+    const testEx = "sue@sue.com";
+    const newEmp = new Employee('Foo', 1, testEx);
+    expect(newEmp.email).toBe(testEx);
+})
+
+
+
